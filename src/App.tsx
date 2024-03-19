e7d009c5b06b80e2363ec9686509b57d7159ebda
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2024-03-19 12:33:01 -0400

    updated pictures

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { EcoservCarousel } from './components/carousel/EcoservCarousel'
 import { EcoservNavbar } from './components/header/EcoservNavbar'
 import logoSrc from './assets/coserv-high-resolution-logo-transparent.png';
-import { EcoservImageList } from './business/services/EcoservImageList';
 import { EcoservFooter } from './components/footer/EcoservFooter';
 import { TestGrid } from './business/services/TestGrid';
 import { itemData } from './data/services_data/ServicesData';
@@ -14,9 +13,6 @@ export const App = () => {
     <>
       <EcoservNavbar logoSrc={logoSrc} />
       <EcoservCarousel />
-      {/* <EcoserviceSection /> */}
-      {/* <EcoservImageList /> */}
-
       <TestGrid images={itemData} />
       <EcoservFooter />
     </>
--- src/data/services_data/ServicesData.tsx
+++ src/data/services_data/ServicesData.tsx
@@ -1,35 +1,30 @@
 export const itemData = [
     {
-        img: 'https://kaizenaire.com/wp-content/uploads/2023/12/Commercial-Cleaning-Services-in-Singapore-Keeping-Your-Business-Sparkling-Clean-1024x585.jpg',
-        title: 'Commercial',
+        img: 'https://oceanicknettoyage.com/wp-content/uploads/2023/09/soins-adaptes-1.png',
+        title: 'Résidentiel',
         author: '',
         rows: 4,
         cols: 2,
         featured: true,
     },
     {
-        img: 'https://thinkdifferentnetwork.com/wp-content/uploads/2019/03/Chalet-style-in-the-interior-of-apartments-and-houses3.jpg',
-        title: 'Résidentiel',
+        img: 'https://kaizenaire.com/wp-content/uploads/2023/12/Commercial-Cleaning-Services-in-Singapore-Keeping-Your-Business-Sparkling-Clean-1024x585.jpg',
+        title: 'Commercial',
         author: '',
     },
     {
-        img: 'https://lirp.cdn-website.com/a2cc572e/dms3rep/multi/opt/man-cleaning-walls-floor-with-high-pressure-640w.jpeg',
-        title: 'Patio',
+        img: 'https://thinkdifferentnetwork.com/wp-content/uploads/2019/03/Chalet-style-in-the-interior-of-apartments-and-houses3.jpg',
+        title: 'Chalet/Airbnb',
         author: '',
-        cols: 2,
     },
     {
-        img: 'https://thinkdifferentnetwork.com/wp-content/uploads/2019/03/Chalet-style-in-the-interior-of-apartments-and-houses3.jpg',
-        title: 'Chalet',
-        author: '/',
-    },
-    {
-        img: 'https://thinkdifferentnetwork.com/wp-content/uploads/2019/03/Chalet-style-in-the-interior-of-apartments-and-houses3.jpg',
+        img: 'https://medias.kiloutou.fr/trs/1982/1320/fit/kite/output/v2/content/media-library/blog_comment_reussir_travaux_fin_chantier_main_c7c72a3696.jpg',
         title: 'Aprés construction',
-        author: '/',
+        author: '',
     },
     {
-        img: 'https://thinkdifferentnetwork.com/wp-content/uploads/2019/03/Chalet-style-in-the-interior-of-apartments-and-houses3.jpg',
+        img: 'https://souaclean.fr/wp-content/uploads/2021/12/nettoyage-fin-chantier-Var-83.jpg',
         title: 'Courtier Immobilier',
-        author: '/',
-    },]
+        author: '',
+    },
+   ]
